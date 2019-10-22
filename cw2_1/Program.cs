1be1735ed7a8a9da69a548096ddc357052717b39
unknown <local@WMII-N6A8FKG85J.wmii.local>
2019-10-22 16:09:03 +0200

    Dodaj pliki projektów.

---
+++ cw2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cw2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
