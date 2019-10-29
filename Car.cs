d4bad1e03f77ab61d65ddbd7082ac21a8479ec5f
PiotrSter <47736359+PiotrSter@users.noreply.github.com>
2019-10-29 17:52:38 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw3
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        static int iloscKol;
+
+        private Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+
+        public static Car Create(double pojemnoscSilnika, string marka)
+        {
+            return new Car(pojemnoscSilnika, marka);
+        }
+
+        static Car()
+        {
+            iloscKol = 4;
+        }
+        
+        ~Car()
+        {
+            MessageBox.Show("Zwalniam pamięć");
+        } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Wyjatki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1, liczba2 = 30, liczba3 = 0 ;
+            int[] table = new int [2];
+            try
+            {
+               // liczba1 = liczba2 / liczba3;
+                table[25] = 2;
+                throw new IndexOutOfRangeException();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
