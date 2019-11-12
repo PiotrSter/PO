75183a4f7ab458e5004109fa9dc751c8c08a77f4
PiotrSter <47736359+PiotrSter@users.noreply.github.com>
2019-11-12 17:35:23 +0100

    Add files via upload

---
+++ CW6/Pracownik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW6
+{
+    class Pracownik
+    {
+        public virtual void Pracuj()
+        {
+            Console.WriteLine("logowanie");
+        }
+    }
+}
---
+++ CW6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pracownik> pracownicy = new List<Pracownik>();
+            pracownicy.Add(new Programista());
+            pracownicy.Add(new Projektant());
+            pracownicy.Add(new Ksiegowy());
+
+            foreach(Pracownik i in pracownicy)
+            {
+                Console.WriteLine($"{i}");
+                i.Pracuj();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
