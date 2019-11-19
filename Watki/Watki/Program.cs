71b0aaf8c253e909f5f17e22e5269bad20e9d1d8
PiotrSter <47736359+PiotrSter@users.noreply.github.com>
2019-11-19 17:40:05 +0100

    Add files via upload

---
+++ Watki/Watki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Watki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thr = new Thread(Licz);
+            thr.Start();
+
+            for(int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine($"Program1:{i}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void Licz()
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine($"Wątek:{i}");
+            }
+        }
+    }
+}
